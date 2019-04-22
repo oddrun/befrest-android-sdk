7e58974b504a48af73e1ad30da2f83f9d1efdb20
Mohamad Hossein <mhyamininia@gmail.com>
2019-04-22 07:17:50 +0430

    implement befrest log to handle log level

---
+++ befrest/src/main/java/bef/rest/befrest/utils/BefrestLog.java
@@ -0,0 +1,40 @@
+package bef.rest.befrest.utils;
+
+import android.util.Log;
+
+import bef.rest.befrest.clientData.ClientData;
+
+public class BefrestLog {
+
+    private static String TAG = "Befrest-[ ";
+
+    private static int getLogLevel() {
+        return ClientData.getInstance().getLogLevel();
+    }
+
+    public static void i(String tag, String message) {
+        if (getLogLevel() < SDKConst.LOG_LEVEL_INFO)
+            Log.i(TAG + tag + " ]", message);
+    }
+
+    public static void e(String tag, String message) {
+        if (getLogLevel() <= SDKConst.LOG_LEVEL_ERROR)
+            Log.e(TAG + tag + " ]", message);
+    }
+
+    public static void v(String tag, String message) {
+        if (getLogLevel() <= SDKConst.LOG_LEVEL_VERBOSE)
+            Log.v(TAG + tag + " ]", message);
+    }
+
+    public static void w(String tag, String message) {
+        if (getLogLevel() <= SDKConst.LOG_LEVEL_WARN)
+            Log.w(TAG + tag + " ]", message);
+    }
+
+    public static void d(String tag, String message) {
+        if (getLogLevel() <= SDKConst.LOG_LEVEL_DEBUG)
+            Log.d(TAG + tag + " ]", message);
+    }
+
+}
